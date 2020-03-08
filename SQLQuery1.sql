b06a10a4992587cca62995a8090db717989cf763
SohaybKelany <s.sohayb.kelany@gmail.com>
2020-03-08 19:39:17 +0200

    1st commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO  CitiesTable(CityID, CityName) VALUES(1, 'Jenin')
+INSERT INTO  CitiesTable(CityID, CityName) VALUES(2, 'Tullkarm')
+INSERT INTO  CitiesTable(CityID, CityName) VALUES(3, 'Nablus')
+INSERT INTO  CitiesTable(CityID, CityName) VALUES(4, 'Rammallah')
+INSERT INTO  CitiesTable(CityID, CityName) VALUES(5, 'Hebron')
